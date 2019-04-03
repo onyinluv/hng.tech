a24ed4cbd6b6a1259a9c11c0a56df557b0e398fa
Onyinyechi <47834490+onyinluv@users.noreply.github.com>
2019-04-03 21:35:57 +0100

    Add files via upload

---
+++ views/interns/onyinluv.php
@@ -0,0 +1,39 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/onyinluv/image/upload/c_scale,h_81,w_81/v1554307874/Onyinluv.jpg" alt="Augusta Kalu">                     
+                         <h6>Augusta Kalu</h6>
+                        <p>UI/UX Designer/Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/onyinyeee" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/onyinluv" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@onyinyechi.akalu" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>Kalu, Augusta Onyinyechi, is a UI/UX developer.
+                        She has a positive demeanor and a very fast learner
+                        She has distinguished herself by taking various online courses and certifications</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | PHP | MySQL </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
